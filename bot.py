75cf3dec48b58a8a673eb4b2d4955e86e5354e52
Liam Svanåsbakken Crouch <spam@petterroea.com>
2018-01-10 18:52:28 +0100

    ok bot works with webhooks

--- bot.py
+++ bot.py
@@ -1,48 +1,101 @@
 # Infected discord bot script
 
 import argparse
-import re
-import discord
-import sys
+import requests, json
 import time
 
 parser = argparse.ArgumentParser(description="Complains about errors to the discord server.")
 parser.add_argument("errorFile", metavar="errorFile", type=str, help="The file to follow.")
-parser.add_argument("discordClientToken", metavar="token", type=str, help="The discord bot client token")
+parser.add_argument("webtokenUrl", metavar="token", type=str, help="The discord bot client token")
 
 args = parser.parse_args()
 
 errorFile = args.errorFile
-clientToken = args.discordClientToken
+webtoken = args.webtokenUrl
 
-# Fire up discord bot
-client = discord.Client()
+def sendNotification(errMsg):
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-    talk_channel = 0
-    for channel in client.get_all_channels():
-    	print("Name: %s, id: %s" % (channel.name, channel.id))
-    	if channel.name == "errors":
-    		talk_channel = channel
-    		break
-    await client.send_message(talk_channel, 'Discord bot is ON')
-    # Follow the file
-    with open(errorFile) as file_:
-            # Go to the end of file
-            file_.seek(0,2)
-            while True:
-                curr_position = file_.tell()
-                line = file_.readline()
-                if not line:
-                    file_.seek(curr_position)
-                    time.sleep(0.1)
-                else:
-                    await client.send_message(talk_channel, 'New error message: \n```\n%s\n```' % line)
-                    #print(line)
+    print("Got message %s" % errMsg)
 
-client.run(clientToken)
+    payload = {
+        "username": "Loggine",
+        # "avatar_url": "",
+        "tts": False,
+        "embeds": [{
+            "title": "New log event",
+            #"url": entry["url"],
+            # "timestamp": "",
+            # "color": "",
+            #"footer": {},
+            #"image": {
+            #    "url": entry["imageUrl"],
+            #},
+            #"thumbnail": {
+            #    "url": entry["imageUrl"],
+            #},
+            #"author": {
+            #    "name": data["sellerName"],
+            #    # Maybe use gravatar when default avatar?
+            #    "icon_url": "https://s.yimg.jp/images/serp/as/ic_prof_default.png",
+            #    # "proxy_icon_url": "",
+            #},
+            #"fields": [
+            #    {
+            #        "name": "Time Remaining",
+            #        "value": data["daysLeft"],
+            #        "inline": True
+            #    },
+            #    {
+            #        "name": "Closing Time",
+            #        "value": data["endTime"],
+            #        "inline": True
+            #    },
+            #    {
+            #        "name": "Early Finish",
+            #        "value": data["earlyTermination"],
+            #        "inline": True
+            #    },
+            #    {
+            #        "name": "Automatic Extension",
+            #        "value": data["autoExtend"],
+            #        "inline": True
+            #    },
+            #    {
+            #        "name": "Current Bid",
+            #        "value": data["bidStr"],
+            #        "inline": True
+            #    },
+            #    {
+            #        "name": "Buy-out Price",
+            #        "value": data["buyoutStr"],
+            #        "inline": True
+            #    },
+            #    {
+            #        "name": "Search term",
+            #        "value": ", ".join(entry["keywordList"])
+            #    }
+            #],
+            "description": "```%s```" % errMsg
+        }]
+    }
+    #print(json.dumps(payload))
+
+    print("Sending to %s" % webtoken)
+
+    r = requests.post(webtoken, data=json.dumps(payload), headers={"Content-Type": "application/json"})
+    time.sleep(0.5)
+
+print("Error log watcher hello world")
+
+with open(errorFile) as file_:
+    # Go to the end of file
+    file_.seek(0,2)
+    while True:
+        curr_position = file_.tell()
+        line = file_.readline()
+        if not line:
+            file_.seek(curr_position)
+            time.sleep(0.1)
+        else:
+            sendNotification(line)
+            #print(line)
