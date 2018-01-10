e07e6cc88fa8df2545a624216d701e729f7e10f5
Liam Svanåsbakken Crouch <spam@petterroea.com>
2018-01-10 19:19:01 +0100

    now with regex

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import argparse
 import requests, json
 import time
+import re
 
 parser = argparse.ArgumentParser(description="Complains about errors to the discord server.")
 parser.add_argument("errorFile", metavar="errorFile", type=str, help="The file to follow.")
@@ -17,67 +18,125 @@ def sendNotification(errMsg):
 
     print("Got message %s" % errMsg)
 
-    payload = {
-        "username": "Loggine",
-        # "avatar_url": "",
-        "tts": False,
-        "embeds": [{
-            "title": "New log event",
-            #"url": entry["url"],
-            # "timestamp": "",
-            # "color": "",
-            #"footer": {},
-            #"image": {
-            #    "url": entry["imageUrl"],
-            #},
-            #"thumbnail": {
-            #    "url": entry["imageUrl"],
-            #},
-            #"author": {
-            #    "name": data["sellerName"],
-            #    # Maybe use gravatar when default avatar?
-            #    "icon_url": "https://s.yimg.jp/images/serp/as/ic_prof_default.png",
-            #    # "proxy_icon_url": "",
-            #},
-            #"fields": [
-            #    {
-            #        "name": "Time Remaining",
-            #        "value": data["daysLeft"],
-            #        "inline": True
-            #    },
-            #    {
-            #        "name": "Closing Time",
-            #        "value": data["endTime"],
-            #        "inline": True
-            #    },
-            #    {
-            #        "name": "Early Finish",
-            #        "value": data["earlyTermination"],
-            #        "inline": True
-            #    },
-            #    {
-            #        "name": "Automatic Extension",
-            #        "value": data["autoExtend"],
-            #        "inline": True
-            #    },
-            #    {
-            #        "name": "Current Bid",
-            #        "value": data["bidStr"],
-            #        "inline": True
-            #    },
-            #    {
-            #        "name": "Buy-out Price",
-            #        "value": data["buyoutStr"],
-            #        "inline": True
-            #    },
-            #    {
-            #        "name": "Search term",
-            #        "value": ", ".join(entry["keywordList"])
-            #    }
-            #],
-            "description": "```%s```" % errMsg
-        }]
-    }
+    regex = "\[([a-zA-Z0-9: .]*)\] \[([a-zA-Z0-9: .]*)\] \[([a-zA-Z0-9: .]*)\] \[client ([a-zA-Z0-9: .]*)\] ([a-zA-Z ]*): *([^\/]*)in ([\/a-zA-Z._:0-9]*)\\\\nStack trace:\\\\n([a-zA-Z0-9#\/ ._():\\'{}]*\\\\n)* *thrown in ([a-zA-Z0-9\/._ ]*), referer: (.*)"
+
+    result = re.match(regex, errMsg)
+
+    if result:
+        payload = {
+            "username": "Loggine",
+            # "avatar_url": "",
+            "tts": False,
+            "embeds": [{
+                "title": "New log event",
+                #"url": entry["url"],
+                # "timestamp": "",
+                # "color": "",
+                #"footer": {},
+                #"image": {
+                #    "url": entry["imageUrl"],
+                #},
+                #"thumbnail": {
+                #    "url": entry["imageUrl"],
+                #},
+                #"author": {
+                #    "name": data["sellerName"],
+                #    # Maybe use gravatar when default avatar?
+                #    "icon_url": "https://s.yimg.jp/images/serp/as/ic_prof_default.png",
+                #    # "proxy_icon_url": "",
+                #},
+                "fields": [
+                    {
+                        "name": "Date",
+                        "value": result.group(1),
+                        "inline": True
+                    },
+                    {
+                        "name": "Client",
+                        "value": result.group(4),
+                        "inline": True
+                    },
+                    {
+                        "name": "Type",
+                        "value": result.group(5),
+                        "inline": True
+                    },
+                    {
+                        "name": "Exception",
+                        "value": result.group(6),
+                        "inline": True
+                    },
+                    {
+                        "name": "File",
+                        "value": result.group(7),
+                        "inline": True
+                    }
+                ]#,
+                #description": "```%s```" % errMsg
+            }]
+        }
+    else:
+        payload = {
+            "username": "Loggine",
+            # "avatar_url": "",
+            "tts": False,
+            "embeds": [{
+                "title": "New log event",
+                #"url": entry["url"],
+                # "timestamp": "",
+                # "color": "",
+                #"footer": {},
+                #"image": {
+                #    "url": entry["imageUrl"],
+                #},
+                #"thumbnail": {
+                #    "url": entry["imageUrl"],
+                #},
+                #"author": {
+                #    "name": data["sellerName"],
+                #    # Maybe use gravatar when default avatar?
+                #    "icon_url": "https://s.yimg.jp/images/serp/as/ic_prof_default.png",
+                #    # "proxy_icon_url": "",
+                #},
+                #"fields": [
+                #    {
+                #        "name": "Time Remaining",
+                #        "value": data["daysLeft"],
+                #        "inline": True
+                #    },
+                #    {
+                #        "name": "Closing Time",
+                #        "value": data["endTime"],
+                #        "inline": True
+                #    },
+                #    {
+                #        "name": "Early Finish",
+                #        "value": data["earlyTermination"],
+                #        "inline": True
+                #    },
+                #    {
+                #        "name": "Automatic Extension",
+                #        "value": data["autoExtend"],
+                #        "inline": True
+                #    },
+                #    {
+                #        "name": "Current Bid",
+                #        "value": data["bidStr"],
+                #        "inline": True
+                #    },
+                #    {
+                #        "name": "Buy-out Price",
+                #        "value": data["buyoutStr"],
+                #        "inline": True
+                #    },
+                #    {
+                #        "name": "Search term",
+                #        "value": ", ".join(entry["keywordList"])
+                #    }
+                #],
+                "description": "```%s```" % errMsg
+            }]
+        }
     #print(json.dumps(payload))
 
     print("Sending to %s" % webtoken)
